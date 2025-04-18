ad8df2478c137c63f17db002799f1dc426c3a69b
Vishal917978 <vishalsharma917978@gmail.com>
2025-04-18 13:51:41 +0530

    first commit

---
+++ Que11.js
@@ -0,0 +1,45 @@
+//automorphic number
+
+// let number=25;
+// let numSqr=number**2;
+// let lastDigit=numSqr%10;
+// if(number==lastDigit){
+//     console.log("automorphic")
+// }else{
+//     console.log("non automorphic")
+// }
+
+
+//harshad number
+
+// let num=52;
+// let dup=num;
+// let sum=0
+// while(num>0){
+//     let rem=num%10;
+//     sum=sum+rem;
+//     num=parseInt(num/10);
+// }
+// if(dup%sum==0){
+//     console.log("harshad")
+// }else{
+//     console.log("not")
+// }
+
+//abundent number
+
+// let num=6;
+// let sum=0;
+// for(let i=1;i<num;i++){
+//     if(num%i==0){
+//      sum=sum+i;
+//     }
+// }
+// if(sum>num){
+//     console.log("abundent number")
+// }else{
+//     console.log("not abundent")
+// }
+
+
+
