05f1d47644506678e2b5c0a29049962e085d3b39
Vishal917978 <vishalsharma917978@gmail.com>
2025-04-21 13:00:06 +0530

    first commit

---
+++ Que12.js
@@ -0,0 +1,78 @@
+//1..hcf of two number
+
+// let num1=15;
+// let num2=24;
+// let hcf=1;
+// for(let i=1;i<=num2;i++){
+//     if(num1%i==0 && num2%i==0){
+//         hcf=i;
+//     }
+// }
+// console.log(hcf)
+
+
+//2..lcm of two number
+
+
+// let n1=6;
+// let n2=4;
+// let lcm=1
+// for(let i=1;i<=n1;i++){
+//     if(i%n1==0 && i%n2==0){
+//       lcm=i;   
+//     }else{
+//         lcm=n1*n2;
+//     }
+// }
+// console.log(lcm);
+
+//3..greatest common divisor 
+
+// let a=120;
+// let b=24;
+// let gcf=1;
+// for(let i=1;i<a;i++){
+//     if(a%i==0 && b%i==0){
+//         gcf=i
+//     }
+// }
+// console.log(gcf)
+
+
+//4..decimal to binary
+
+// let num=8;
+// let binary="";
+// while(num>0){
+//     let a=num%2;
+//     binary=a+binary
+//     num=parseInt(num/2)
+// }
+// console.log(binary)
+
+
+// 5..binary to decimal
+
+// let num=101;
+// let count=0;
+// let ans=0
+// while(num>0){
+//     let a=num%10;
+//     ans=ans+2**count*a
+//     count=count+1;
+//     num=parseInt(num/10)
+// }
+// console.log(ans)
+
+
+
+
+
+
+
+
+
+
+
+
+
