55a9894be52abb8335ed00ab498463d87b7da50b
Vishal917978 <vishalsharma917978@gmail.com>
2025-04-22 12:31:22 +0530

    Add Que13.js

---
+++ Que13.js
@@ -0,0 +1,76 @@
+//1...decimal to octal
+
+// let num=172;
+// let oct="";
+// while(num>0){
+//     let rem=num%8;
+//     oct=rem+oct;
+//     num=parseInt(num/8);
+// }
+// console.log(oct)
+
+//2...octal to decimal
+
+// let num=254;
+// let deg=0;
+// let dec=0;
+// while(num>0){
+//     let rem=num%10;
+//     dec=dec+rem*(8**deg);
+//     deg++;
+//     num=parseInt(num/10);
+// }
+// console.log(dec)
+
+//3...binary to octal
+
+
+// let num=111;
+// let deg=0
+// let oct=0;
+// while(num>0){
+//     let rem=num%10;
+//     oct=oct+rem*2**deg
+//     deg++;
+//     num=parseInt(num/10)
+// }
+// console.log(oct)
+
+//4..octal to binary
+
+// let num=7;
+// let binary=""
+// while(num>0){
+//     let rem=num%2;
+//     binary=binary+rem;
+//     num=parseInt(num/2)
+// }
+// console.log(binary)
+
+// 5..which coordinates the points lies
+
+// function quadrant(a,b){
+//     if(a>0 && b>0){
+//         console.log("1st")
+//     }else if(a<0 && b>0){
+//         console.log("2nd")
+//     }else if(a<0 && b<0){
+//         console.log("3rd")
+//     }else{
+//         console.log("4th")
+//     }
+// }
+// quadrant(-4,5)
+
+
+
+
+
+
+
+
+
+
+
+
+
