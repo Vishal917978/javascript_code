b7dd9490ffcf3b038b9c12d6f7b86cc56eea48b4
Vishal917978 <vishalsharma917978@gmail.com>
2025-04-23 12:21:00 +0530

    Add new JavaScript file

---
+++ Que14.js
@@ -0,0 +1,28 @@
+//how many way n people can sit on r chairs
+
+// let seat=3;
+// let man=5;
+// function fact(val){
+// let fact=1;
+//  for(let i=1;i<=val;i++){
+//     fact=fact*i;
+// }   
+// console.log(fact)
+// return fact
+// }
+
+// let a=fact(man);
+// let b=fact(man-seat);
+// console.log(parseInt(a/b))
+
+//maximum handshakes
+
+// let p=110;
+// let res=p*(p-1)/2
+// console.log(res)
+
+//fractions sum
+
+// let a=1/2;
+// let b=1/4;
+// console.log((a+b));
