10a870839ffdc0783ebb1fd72b684791401f20aa
Vishal917978 <vishalsharma917978@gmail.com>
2025-04-27 17:54:24 +0530

    Initial commit

---
+++ Que17.js
@@ -0,0 +1,39 @@
+//hcf using recursion
+
+// 1...function hcf(a,b){
+//     if(b==0){
+//         return a;
+//     }
+//     return hcf(b,a%b);
+// }
+// let res=hcf(8,6);
+// console.log(res);
+
+
+//lcm using recursion
+
+//2... function hcf(a,b){
+//     if(b==0){
+//         return a;
+//     }
+//     return hcf(b,a%b)
+// }
+// let a=6;
+// let b=8;
+// let res=hcf(a,b);
+// console.log(res)
+// let lcm=parseInt((a*b)/res);
+// console.log(lcm);
+
+//3...lengrh of string using recursion
+
+
+// function len(str,count){
+//     if(count==str.length){
+//         return count;
+//     }
+//     return len(str,count+=1)
+// }
+// let str="vishal"
+// let res=len(str ,0)
+// console.log(res)
