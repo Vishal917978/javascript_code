bd453a076f37c756596afe43eabe83e8e25915ea
Vishal917978 <vishalsharma917978@gmail.com>
2025-05-05 12:33:58 +0530

    Initial commit

---
+++ Que20.js
@@ -0,0 +1,33 @@
+//1...unique elements in array
+
+// let arr=[1,2,3,8,2,4,3,9,4,10,11,10];
+// let dup=[];
+// for(let i=0;i<arr.length;i++){
+//     let count=0
+//     for(let j=0;j<arr.length;j++){
+//         if(arr[i]==arr[j]){
+//         count++;
+//         }
+//     }
+//     if(count==1){
+//         dup.push(arr[i])
+//     }
+// }
+// console.log(dup);
+
+//2...repeating element in array
+
+// let arr=[1,2,3,2,4,5,3];
+// let dup=[]
+// for(let i=0;i<arr.length;i++){
+//     let count=0;
+//     for(let j=i;j<arr.length;j++){
+//         if(arr[i]==arr[j]){
+//             count++;
+//         }
+//     }
+//     if(count==2){
+//         dup.push(arr[i])
+//     }
+// // }
+// console.log(dup)
