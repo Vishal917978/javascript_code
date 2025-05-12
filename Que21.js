f77158179a36b30dd0a2a24fcff4fbe4c894aa81
Vishal917978 <vishalsharma917978@gmail.com>
2025-05-12 12:56:03 +0530

    Initial commit

---
+++ Que21.js
@@ -0,0 +1,39 @@
+//1..even and odd digit count
+// let arr=[1,2,3,4,5,6,7];
+// let count=0;
+// let count1=0;
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]%2==0){
+//         count+=1;
+//     }
+//     else{
+//         count1+=1;
+//     }
+// }
+// console.log("total even digit :",count);
+// console.log("total odd digit :",count1);
+
+//2..array is disjoint or not
+//(disjoint--> no ele match in two array)
+
+// let a=[1,2,3,4];
+// let b=[14,5,6];
+// let flag=true;
+// for(let i=0;i<a.length;i++){
+//     for(let j=0;j<b.length;j++){
+//         if(a[i]==b[j]){
+//             flag=false;
+//         }
+//     }
+    
+// }
+// console.log(flag);
+
+//3..replace array element with its rank
+//(rank --> position of array ele after sorting )
+
+// let arr=[11,12,13,14,15];
+// for(let i=0;i<arr.length;i++){
+//     arr[i]=i+1;
+// }
+// console.log(arr);
