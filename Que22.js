6b57b734923eea6d61e64e5fab525ccbd72b1e15
Vishal917978 <vishalsharma917978@gmail.com>
2025-05-13 12:51:54 +0530

    Initial commit

---
+++ Que22.js
@@ -0,0 +1,34 @@
+//1...scaller product of array
+
+// let arr=[1,2,3];
+// let arr1=[2,3,4];
+// let product=[];
+// for(let i=0;i<arr.length;i++){
+//     product.push(arr[i]*arr1[i])
+// }
+// console.log(product)
+
+//2...min scller product
+
+// let arr=[1,3,2];
+// let arr1=[5,6,4];
+// let product=0;
+// for(let i=0;i<arr.length;i++){
+//     let n=arr.sort((a,b)=>a-b);
+//     let m=arr1.sort((a,b)=>a-b);
+//     product+=n[i]*m[i];
+// }
+// console.log(product)
+
+
+//3...max scaller product
+
+// let arr=[1,3,2];
+// let arr1=[5,6,4];
+// let product=0;
+// for(let i=0;i<arr.length;i++){
+//     let n=arr.sort((a,b)=>a-b);
+//     let m=arr1.sort((a,b)=>b-a);
+//     product+=n[i]*m[i];
+// }
+// console.log(product)
