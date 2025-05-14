49e4f56fb001916c68aed055490900c774f7a8ef
Vishal917978 <vishalsharma917978@gmail.com>
2025-05-14 14:06:23 +0530

    initial commit

---
+++ Que23.js
@@ -0,0 +1,42 @@
+//1...check char is upr or lower case
+
+// let char="a";
+// let res=char.charCodeAt(0);
+// if(res>=65 && res<=90){
+//     console.log("uper case character",res)
+// }
+// else if(res>=97 && res<=122){
+//     console.log("lower case",res)
+// }
+
+//2...check char is vowel or consonent
+
+// let char1="A";
+// let char=char1.toLowerCase();
+// if(char=="a" || char=="e"||char=="i"||char=="o"||char=="u"){
+//     console.log("vowel")
+// }
+// else{
+//     console.log("consonent")
+// }
+
+//3...find ascii value of character
+
+// let char="z"
+// let ascii=char.charCodeAt(0);
+// console.log("ascii value of char is :",ascii)
+
+//4...length without using len function
+
+// let str="vishal sharma";
+// let count=0;
+// for(let i in str){
+//     if(str[i]!=" "){
+//         count++;
+//     }
+// }
+// console.log("length is:",count)
+
+
+
+
