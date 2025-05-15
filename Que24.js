4ea38c264d7b60b56b141ee4a899c84d9ddadce7
Vishal917978 <vishalsharma917978@gmail.com>
2025-05-15 14:39:41 +0530

    initial commit

---
+++ Que24.js
@@ -0,0 +1,11 @@
+//2nd method 
+// let arr = [1, 2, 3, 4, 5, 1, 2];
+// for(let i=0;i<=arr.length-1;i++){
+//     let count=0;
+//     for (let j=0;j<=arr.length-1;j++){
+//         if(arr[i]==arr[j]){
+//             count++;
+//         }
+//     }
+//     console.log(arr[i]+"=>",count);
+// }
