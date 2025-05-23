4a9fe8dedbfbc7e02bce847b5a1abba21c30379d
Vishal917978 <vishalsharma917978@gmail.com>
2025-05-23 12:56:22 +0530

    initial commit

---
+++ Que26.js
@@ -0,0 +1,31 @@
+// object methods
+// 1.Object.assign-->
+
+// let target={a:1,b:2,c:3};
+// let des={d,:4,5:6};
+// Object.assign(target,des);
+// console.log(target);
+
+// 2.Object.entries-->
+
+// let target={a:1,b:2};
+// let res=Object.entries();
+// console.log(res);
+
+// 3.Object.fromEntries-->
+
+// let arr=[["a",1],["b",2],["c",3]];
+// let res=Object.fromEntries(arr);
+// console.log(res);
+
+// 4.Object.keys-->
+
+// let obj1={name:"vishal",college:"medicaps",age:22};
+// let key=Object.keys(obj1);
+// console.log(key);
+
+// 5.Object.values-->
+
+// let obj1={name:"vishal",college:"medicaps",age:22};
+// let value=Object.values(obj1);
+// console.log(value);
