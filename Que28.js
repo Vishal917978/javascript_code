720aa09316ad15fd3f2458b2aeafd4391ea712be
Vishal917978 <vishalsharma917978@gmail.com>
2025-05-28 22:00:56 +0530

    initial commit

---
+++ Que28.js
@@ -0,0 +1,59 @@
+//1...length
+
+// let str="vishal sharma a";
+// let count=0;
+// for(let i=0;i<str.length;i++){
+//     if(str[i]!=" "){
+//         count++;
+//     }
+// }
+// console.log(count);
+
+//2...slice
+
+// let str="vishalsharma"
+// console.log(str.slice(0,5))
+// console.log(str.slice(7))
+// console.log(str.slice(-5))
+
+//3...substring
+
+// let str="vishalsharma";
+// console.log(str.substring(4))
+
+//4...concat
+// let str="vishal";
+// let str2="sharma"
+// console.log(str.concat(str2))
+
+//5...replace
+// let str="vishal";
+// console.log(str.replace('a','w'))
+
+//6...replaceAll
+
+// let str="vishalsharma";
+// console.log(str.replaceAll('a','q'))
+
+//7...split
+
+// let str="vishal sharma";
+// console.log(str.split(" "));
+
+//8....charAt
+
+// let str="vishal"
+// console.log(str.charAt(4));
+
+//9...charCodeAt
+
+// let str="sharmaji";
+// console.log(str.charCodeAt(3));
+
+//10...includes
+
+// let str="vishal";
+// console.log(str.includes("a"))
+
+
+
