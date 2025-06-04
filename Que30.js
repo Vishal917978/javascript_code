95ab83ee39fca47955d1e59c7d7cfd6ca830d9d5
Vishal917978 <vishalsharma917978@gmail.com>
2025-06-04 14:31:51 +0530

    initial commit

---
+++ Que30.js
@@ -0,0 +1,50 @@
+//1....pattern 1
+
+// let n=5;
+// for(let i=1;i<=n;i++){
+//     let str=""
+//     for(let j=1;j<=i;j++){
+//       str+="*"
+//     }
+//     console.log(str)
+// }
+
+//2...pattern 2
+
+// let n=5;
+// for(let i=1;i<=n;i++){
+//     let str=""
+//     for(let j=1;j<=n-i+1;j++){
+//         str+="*"
+//     }
+//     console.log(str)
+// }
+
+//3...pattern 3
+
+// let n=5;
+// for(let i=1;i<=n;i++){
+//     let str=""
+//     for(let j=1;j<=n-i;j++){
+//       str+=" "
+//     }
+//     for(let k=n-i+1;k<=n;k++){
+//         str+="*"
+//     }
+//     console.log(str)
+// }
+
+//4...pattern 4
+
+// let n=5;
+// for(let i=1;i<=n;i++){
+//     let str="";
+//     for(let j=1;j<i;j++){
+//         str+=" "
+//     }
+//     for(let k=1;k<=n-i+1;k++){
+//         str+="*"
+//     }
+//     console.log(str)
+// }
+
