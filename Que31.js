5ad1191c365daaead612cbc9f2e07991797111fa
Vishal917978 <vishalsharma917978@gmail.com>
2025-06-25 00:47:02 +0530

    initail commit

---
+++ Que31.js
@@ -0,0 +1,45 @@
+//1.pattern 1
+
+// let n=4;
+// for(let i=1;i<=n;i++){
+//     let str=""
+//     for(let j=1;j<=i;j++){
+//         str+="*"
+//     }
+//     console.log(str)
+// }
+
+//2.pattern 2
+
+
+// let n=4;
+// for(let i=1;i<=4;i++){
+//     let str=""
+//     for(let j=1;j<=n-i+1;j++){
+//         str+="*"
+//     }
+//     console.log(str)
+// }
+
+//3.pattern 3
+
+// let n=4;
+// for(let i=1;i<=n;i++){
+//     let str=""
+//     for(let j=1;j<=n-i;j++){
+//         str+=" "
+//     }
+//     for(let k=n-i+1;k<=n;k++){
+//         str+="*"
+//     }
+//     console.log(str)
+// }
+
+
+
+
+
+
+
+
+
