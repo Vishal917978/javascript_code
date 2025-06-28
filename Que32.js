6b54cc2f390f8835cfed8631f278b2aa9ffa647a
Vishal917978 <vishalsharma917978@gmail.com>
2025-06-28 17:47:38 +0530

    initial commit

---
+++ Que32.js
@@ -0,0 +1,34 @@
+//1.array 
+
+// let arr=[1,2,3,3,4,56,43];
+// let high=arr[0]
+// for(let i=0;i<arr.length;i++){
+//     let count=0;
+//     for(let j=i;j<arr.length;j++){
+//         if(arr[i]>arr[j]){
+//             count++;
+//         }
+//     }
+//     if(count>0){
+//       high=arr[i]
+//     }
+ 
+//     }
+//     console.log(high)
+   
+   
+   //2.array
+   
+//     let arr=[42,54,6,65,33,23]
+//   let min=arr[0]
+//   for(let i=0;i<arr.length;i++){
+//      if(arr[i]<min){
+//          min=arr[i]
+//      }
+//   }
+//   console.log(min)
+  
+   
+   
+   
+   
