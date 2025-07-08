20d1edf42c223923cf9caff1487a473c4c5a6ebf
Vishal917978 <vishalsharma917978@gmail.com>
2025-07-09 00:29:01 +0530

    initial commit

---
+++ Que35.js
@@ -0,0 +1,32 @@
+//1
+
+// let str="vishal sharma";
+// let count=0;
+// for(let i=0;i<str.length;i++){
+//     if(str[i]!=" "){
+//        count++   
+//     }
+// }
+// console.log(count)
+
+//2.
+
+// let str="vish@!$harm@";
+// let punc_char="!@#$%^&*";
+// let count=0;
+// let temp=""
+// for(let i=0;i<str.length;i++){
+//     count=0;
+//     for(let j=0;j<punc_char.length;j++){
+//         if(str[i]==punc_char[j]){
+//            count++;
+//         }
+//     }
+//   if(count==0){
+//     temp+=str[i]
+//   }
+// }
+// console.log(temp)
+
+
+
