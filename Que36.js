613a98a871494a3a7b6f465621d99abd2da223ad
Vishal917978 <vishalsharma917978@gmail.com>
2025-07-09 17:06:24 +0530

    initial commit

---
+++ Que36.js
@@ -0,0 +1,33 @@
+//1.rotate array
+
+// let arr=[1,2,3,4,5,6,7,8];
+// let s=2;
+// for(let i=0;i<4;i++){
+//     let first=arr[0];
+//     for(let j=0;j<arr.length-1;j++){
+//         arr[j]=arr[j+1]
+//     }
+//     arr[arr.length-1]=first
+// }
+// console.log(arr)
+
+//2.
+
+// let num=12341;
+// let sum=0
+// let finsum=0
+// while(num>0){
+//     let rem=num%10;
+//     sum+=rem;
+//     num=parseInt(num/10)
+// }
+// console.log(sum)
+// if(sum>=10){
+//     while(sum>0){
+//     let rem=sum%10;
+//     finsum+=rem;
+//     sum=parseInt(sum/10)
+// }
+// }
+// console.log(finsum)
+
