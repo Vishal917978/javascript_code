3609bef82fe6c4ae90704865dc1ca8eb46daf640
Vishal917978 <vishalsharma917978@gmail.com>
2025-07-10 16:19:58 +0530

    initial commit

---
+++ Que37.js
@@ -0,0 +1,57 @@
+//1. to find the duplicates in array
+
+// let arr=[1,2,2,3,4,5,3];
+// let dup=[];
+// for(let i=0;i<arr.length;i++){
+//     let count=0;
+//     for(let j=i+1;j<arr.length;j++){
+//         if(arr[i]==arr[j]){
+//           count+=1 
+//         }
+//     }
+//     if(count>0){
+//         dup.push(arr[i])
+//     }
+// }
+// console.log(dup)
+
+
+//2. occurance of each element in array
+
+// let arr = [1, 2, 2, 3, 1, 4, 2];
+// let obj={}
+
+// for(let i=0;i<arr.length;i++){
+//     let ele=arr[i];
+    
+//     if(obj[ele]){
+//         obj[ele]+=1
+//     }else{
+//         obj[ele]=1;
+//     }
+// }
+// console.log(obj)
+
+//3.sort an array without sort
+
+// let arr=[1,2,3,4,5];
+// let arr1=[2,3,6,7,8];
+// let inter=[]
+// for(let i=0;i<arr.length;i++){
+//     let count=0;
+//     for(let j=0;j<arr1.length;j++){
+//         if(arr[i]==arr1[j]){
+//             count+=1;
+//         }
+//     }
+//     if(count>0){
+//         inter.push(arr[i])
+//     }
+// }
+// console.log(inter);
+
+
+
+
+
+
