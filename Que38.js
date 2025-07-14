68950110ade4fc0500adb0209a781d5164207076
Vishal917978 <vishalsharma917978@gmail.com>
2025-07-14 15:53:10 +0530

    initial commit

---
+++ Que38.js
@@ -0,0 +1,49 @@
+//1.sum of even element in array
+
+// let arr=[1,2,3,4,5,6,7,8];
+// let sum=0;
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]%2==0){
+//         sum+=arr[i]
+//     }
+// }
+// console.log(sum)
+
+
+//2.match the array
+// let arr=[1,2,3];
+// let arr1=[1,2,3];
+// let temp=0;
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]==arr1[i]){
+//         temp+=1
+//     }else{
+//         temp=0;
+//     }
+// }
+// if(temp==arr.length){
+//     console.log("both are same")
+// }else{
+//     console.log("different")
+// }
+
+
+//3.move all the zero to end
+
+//  let arr = [1, 2, 0, 4, 0, 6, 0, 9, 8];
+// let count = 0; 
+
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]!=0){
+//         arr[count]=arr[i]
+//         count++;
+//     }
+// }
+// while(count<arr.length){
+//     arr[count]=0
+//     count++
+// }
+// console.log(arr)
+
+
+
