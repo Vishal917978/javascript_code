2cffc9d1cb82d56035afe3bcc2f465131d3d3bba
Vishal917978 <vishalsharma917978@gmail.com>
2025-07-20 00:02:06 +0530

    initial commit

---
+++ Que42.js
@@ -0,0 +1,47 @@
+//1.palindrom string
+
+// let str="naman";
+// let rev=""
+// for(let i=str.length-1;i>=0;i--){
+//     rev+=str[i]
+// }
+// console.log(rev)
+// if(str===rev){
+//     console.log("palindrom")
+// }
+// else{
+//     console.log("not")
+// }
+
+//2.count number of vowels
+
+// let str="vishal sharma";
+// let count=0;
+// let arr=['a','e','i','o','u'];
+// for(let i=0;i<str.length;i++){
+//     for(let j=0;j<arr.length;j++){
+//         if(str[i]==arr[j]){
+//             count++;
+//         }
+//     }
+// }
+// console.log(count)
+
+
+// 3.occurance of each character 
+
+// let str = "hello";
+// let freq = {};
+
+// for (let i = 0; i < str.length; i++) {
+//     let char = str[i];
+
+//     if (freq[char]) {
+//         freq[char] += 1; 
+//     } else {
+//         freq[char] = 1;  
+//     }
+// }
+
+// console.log(freq);
+
