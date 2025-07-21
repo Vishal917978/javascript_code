6ab7ed22205007c0ebb88938660acabe35da058a
Vishal917978 <vishalsharma917978@gmail.com>
2025-07-21 14:50:27 +0530

    initial commit

---
+++ Que43.js
@@ -0,0 +1,66 @@
+//1.sum of each row and print the max
+
+// let arr=[[1,2,3],[4,3,1],[9,8,7]]
+// dup=[];
+// max=0
+// for(let i=0;i<arr.length;i++){
+//     let sum=0;
+//     for(let j=0;j<arr[i].length;j++){
+//         sum=sum+arr[i][j]
+//     }
+//     if(sum>max){
+//         max=sum;
+//     }
+//  dup.push(sum)
+// }
+// console.log(dup)
+// console.log(max)
+
+//2.replace even number with 0
+
+// let arr=[[1,2,3],[4,5,6],[2,4,7]]
+// for(let i=0;i<arr.length;i++){
+//     for(let j=0;j<arr[i].length;j++){
+//         if(arr[i][j]%2==0){
+//             arr[i][j]=0;
+//         }
+//     }
+// }
+// console.log(arr)
+
+
+//3.diagonal sum of matrix
+
+// let arr=[
+//     [1,2,3],
+//     [5,4,8],
+//     [7,4,2]
+//     ]
+// for(let i=0;i<arr.length;i++){
+//     for(let j=0;j<arr[i].length;j++){
+//         if(i==j){
+//             arr[i][j]=0;
+//         }
+//     }
+// }    
+// console.log(arr)
+
+
+//4.swap without using third variable
+
+// let a=10;
+// let b=20;
+// [a ,b] =[b,a];
+// console.log(a)
+// console.log(b)
+
+
+
+
+
+
+
+
+
+
+
