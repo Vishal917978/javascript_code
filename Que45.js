9b02d1f5b2b1dd6de97944213c0fd4f22f511cbe
Vishal917978 <vishalsharma917978@gmail.com>
2025-07-27 13:14:28 +0530

    initial commmit

---
+++ Que45.js
@@ -0,0 +1,46 @@
+//1.largest element
+
+// let arr=[1,12,33,41,5,6]
+// let large=arr[0];
+// let secondLarge=arr[0]
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]>large){
+//         large=arr[i]
+//     }
+// }
+// console.log(large)
+
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]>secondLarge && arr[i]!=large){
+//         secondLarge=arr[i]
+//     }
+// }
+// console.log(secondLarge)
+
+//2.smallest
+
+// let arr=[1,2,3,4,5,6];
+// let small=arr[0];
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]<small){
+//         small=arr[i]
+//     }
+// }
+// console.log(small)
+
+//3.reverse an array
+
+// let arr=[1,2,3,4,5,6]
+// let dup=[]
+// for(let i=arr.length-1;i>=0;i--){
+//     dup.push(arr[i])
+// }
+// console.log(dup)
+
+
+
+
+
+
+
+
