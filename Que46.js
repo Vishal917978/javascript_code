dfb4760e63ca41412981b77131fcf83f0688bca8
Vishal917978 <vishalsharma917978@gmail.com>
2025-08-15 14:59:03 +0530

    initial commit

---
+++ Que46.js
@@ -0,0 +1,44 @@
+// let str="listen";
+// let str2="silent";
+// let sortStr=str.split("").sort().join("");
+// let sortStr2=str2.split("").sort().join("");
+//  console.log(sortStr)
+//  console.log(sortStr2)
+//  if(sortStr==sortStr2){
+//      console.log("anagram")
+//  }
+//  else{
+//      console.log("not")
+//  }
+
+//arr intersection
+
+// let arr=[1,2,3,4,5]
+// let arr2=[5,6,7,2,2]
+// let intr=[];
+// for(let i=0;i<arr.length;i++){
+//     for(let j=0;j<arr2.length;j++){
+//     if(arr[i]==arr2[j] && ! intr.includes(arr[i])){
+//         intr.push(arr[i])
+//     }     
+//     }
+   
+// }
+// console.log(intr)
+
+//arr union
+
+// let arr=[1,2,3,4,5]
+// let arr2=[2,4,8]
+// let res=[]
+// for(let i=0;i<arr.length;i++){
+//     if(!res.includes(arr[i])){
+//         res.push(arr[i])
+//     }
+// }
+// for(let i=0;i<arr2.length;i++){
+//     if(!res.includes(arr2[i])){
+//         res.push(arr2[i])
+//     }
+// }
+// console.log(res)
