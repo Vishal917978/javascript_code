39287466ab3c1a32f9ad9832746b73498e4c8f12
Vishal917978 <vishalsharma917978@gmail.com>
2025-08-26 00:02:42 +0530

    initial commit

---
+++ Que47.js
@@ -0,0 +1,32 @@
+// let n=4;
+// for(let i=1;i<=n;i++){
+//     let str=""
+//     for(let j=1;j<=i;j++){
+//         str+="* "
+//     }
+//     console.log(str)
+// }
+
+
+// let n=4;
+// for(let i=1;i<=n;i++){
+//     let str=""
+//     for(let j=1;j<=n-i+1;j++){
+//         str+="* "
+//     }
+//     console.log(str)
+// }
+
+
+
+// let n=4;
+// for(let i=1;i<=n;i++){
+//     let str="";
+//     for(let j=1;j<=n-i;j++){
+//         str+="  "
+//     }
+//     for(let k=n-i+1;k<=n;k++){
+//         str+="* "
+//     }
+//     console.log(str)
+// }
