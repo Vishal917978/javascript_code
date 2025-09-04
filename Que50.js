a3bd98452eb6a146b2ca26de61c306512b1f2192
Vishal917978 <vishalsharma917978@gmail.com>
2025-09-04 23:52:51 +0530

    initial commit

---
+++ Que50.js
@@ -0,0 +1,36 @@
+// let arr=[1,2,3,4,5,6];
+// let arr2=[7,8,3,2,1,9,8];
+// for(let i=0;i<arr.length;i++){
+//     for(let j=0;j<arr2.length;j++){
+//         if (!arr.includes(arr2[j])){
+//             arr.push(arr2[j])
+//         }
+//     }
+// }
+// console.log(arr)
+
+// let arr=[1,2,3,4,3,2,6,5,3,1];
+// let dup=[]
+// for(let i=0;i<arr.length;i++){
+//     if(dup.includes(arr[i])){
+//         continue;
+//     }
+//     let count=0;
+//     for(let j=0;j<arr.length;j++){
+//         if(arr[i]==arr[j]){
+//             count++
+//         }
+//     }
+//      console.log(arr[i] + " → " + count + " times");
+//     dup.push(arr[i]);
+// }
+
+
+// let arr=[1,2,3,4,5,6,7];
+// for(let i=0;i<arr.length;i++){
+//     for(let j=i+1;j<arr.length;j++){
+//         if(arr[i]+arr[j]==7){
+//             console.log("("+arr[i]+","+arr[j]+")")
+//         }
+//     }
+// }
