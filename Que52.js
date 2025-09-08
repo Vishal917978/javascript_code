04ff8ef4f7db38d61df97ebc2d9a29e437bd738c
Vishal917978 <vishalsharma917978@gmail.com>
2025-09-08 23:49:54 +0530

    initial commit

---
+++ Que52.js
@@ -0,0 +1,40 @@
+// let arr=[1,2,3];
+// let arr2=[4,5,6]
+// for(let i=0;i<arr2.length;i++){
+//     arr.push(arr2[i])
+// }
+// console.log(arr)
+
+
+// let arr=[16,17,4,3,5,2]
+// let new1=[]
+// for(let i=0;i<arr.length;i++){
+//     let count=0;
+//     for(let j=i+1;j<arr.length;j++){
+//         if(arr[i]<arr[j]){
+//             count++
+//         }
+//     }
+//     if(count==0){
+//         new1.push(arr[i])
+//     }
+// }
+// console.log(new1)
+
+// let arr=[1,3,2,4,5]
+// let arr2=[1,2,3,4,5];
+// let flag=0;
+// let count=0
+// for(let i=0;i<arr.length;i++){
+//     if(arr.length==arr2.length){
+//         if(arr[i]==arr2[count]){
+//             flag++;
+//         }
+//     }
+//     count++
+// }
+// if(flag==arr.length){
+//     console.log("eql")
+// }else{
+//     console.log("not eql")
+//}
